59f0c967114cd31fd3f74a61f7cdddbcbfc4e1f9
Lawrence <adelegbelawrence@gmail.com>
2023-02-22 04:40:27 +0100

    setup redus

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TO-DO Tickets</h1>
+      <div></div>
+    </div>
+  )
+}
+
+export default App
